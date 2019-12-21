8107db65bac769c937b88bc7786a22d72a433b72
Lisa <lisa.michael@hotmail.com>
2019-12-21 06:59:07 -0500

    modifying the GET and POST

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,7 +16,8 @@ var friends = require("../data/friends");
 
 module.exports = function(app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
+  // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends. 
+
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
@@ -24,31 +25,35 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friends);
     console.log(friends);
-    console.log("friendsData[0].scores[0]: " + friendsData[0].scores[0]);
+    
 
   });
 
-//   app.get("/api/waitlist", function(req, res) {
-//     res.json(waitListData);
-//   });
+
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
   // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // Then the server saves the data to the friends array)
+
+  //A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
+    
+    
     // req.body is available since we're using the body parsing middleware
 
+      
     if (friends.length < 5) {
       friends.push(req.body);
       res.json(true);
     }
+
+    // else code is to 
     // else {
     //   waitListData.push(req.body);
     //   res.json(false);
@@ -56,13 +61,13 @@ module.exports = function(app) {
   });
 
   // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+  //  this  code will clear out the survey questions
+  
 
   app.post("/api/clear", function(req, res) {
     // Empty out the arrays of data
     friends.length = 0;
-    // waitListData.length = 0;
+    
 
     res.json({ ok: true });
   });

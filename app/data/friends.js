bde0073ac6a4f61ea9f7d7d641c587109632db03
Lisa <lisa.michael@hotmail.com>
2019-12-22 14:54:24 -0500

    adding another friend record to use for testing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,39 @@
 var friends = [{
-    name: "Ahmed",
-     photo :"https://cdn1.thr.com/sites/default/files/imagecache/NFE_portrait/2011/06/lisasimpa_a_p.jpg",
-     scores :[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }]
-  
+  name: "Ahmed",
+  photo: "https://cdn1.thr.com/sites/default/files/imagecache/NFE_portrait/2011/06/lisasimpa_a_p.jpg",
+  scores: [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ]
+},
+
+      {
+  name: "Lisa",
+  photo: "https://www.stickpng.com/assets/images/5a0c40b85a997e1c2cea116e.png",
+  scores: [
+    3,
+    5,
+    3,
+    1,
+    3,
+    4,
+    3,
+    4,
+    1,
+    3
+  ]
+
+}
+];
+
+
 
 module.exports = friends; 

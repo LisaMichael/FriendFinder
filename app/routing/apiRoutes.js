724ad551478b801a05c8fc5dd0f620b9bd842b54
Lisa <lisa.michael@hotmail.com>
2019-12-22 18:18:11 -0500

    working on my nested for loop for friend compatibility logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,6 @@
 // ===============================================================================
 
 let friends = require("../data/friends");
-// var waitListData = require("../data/waitinglistData");
 
 
 // ===============================================================================
@@ -18,24 +17,17 @@ module.exports = function (app) {
   // API GET Requests
   // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends. 
 
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
   app.get("/api/friends", function (req, res) {
     res.json(friends);
-    console.log(friends);
 
 
   });
-
 
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the friends array)
 
   //A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
@@ -58,39 +50,51 @@ module.exports = function (app) {
     res.json(true);
 
     // console.log(friends);
-// console.log(req.body.scores);
+    // console.log(req.body.scores);
 
-var friendScores = req.body.scores; 
-// console.log(friendScores);
+    var friendScores = req.body.scores;
+    // console.log(friendScores);
 
-// create a nested for loop. The outside loop , we will iterate through the friends in the friends array, and in the inside loop, we will loop through the scores
+    // create a nested for loop. 
+    //The outside loop , we will iterate through the friends in the friends array,
+    // and in the inside loop, we will loop through the scores in the friend iteration
 
-for (let i=0; i<friends.length; i++){
-  // console.log('friend = ' + JSON.stringify(friends[i]));
-  
-}
- for (let j=0; j< friendScores.length; j++){
-   console.log(friendScores[j] + " + this is friendsscore"); 
-   console.log(friends[i].scores[j] + " this is friend array score"); 
-}
+    for (let i = 0; i < friends.length; i++) {
+      // console.log('friend = ' + JSON.stringify(friends[i]));
 
 
+      for (let j = 0; j < friendScores.length; j++) {
 
-    // Determine the user's most compatible friend using the following as a guide:
-
-    // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-
-
-    // With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-
-    
+        // Determine the user's most compatible friend using the following as a guide:
 
 
-    // Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
-    // The closest match will be the user with the least amount of difference.
+        // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+
+
+        console.log(friendScores[j] + " + this is friendsscore");
+        console.log(parseInt(friends[i].scores[j]) + " this is friend array score");
+
+        // compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
+
+
+        // Remember to use the absolute value of the differences. Put another way: no negative solutions! 
+        // The closest match will be the user with the least amount of difference.
+
+
+      }
+    }
+
+
+
+
+
+
+
+
 
 
   });
+  
 
   // ---------------------------------------------------------------------------
   //  this  code will clear out the survey questions
